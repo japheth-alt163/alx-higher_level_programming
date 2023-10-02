0780b34046ca4b548c3b6fd16c1e6187597d8f3d
japheth-alt163 <japhethbaraka4@gmail.com>
2023-10-02 07:04:08 -0700

    0-run

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,24 @@
+#ifndef _lists_h
+#define _lists_h
+
+#include <stdlib.h>
+
+/**
+ *struct listint_s - singly linked list
+ *@n: integer
+ *@next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+int n;
+struct listint_s *next;
+} listint_t;
+int check_cycle(listint_t *list);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+#endif

f9aa9a43b8494d7f9b8e3be3647521e64457d1af
japheth-alt163 <japhethbaraka4@gmail.com>
2023-10-07 05:50:47 -0700

    1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
+
+
+# Example usage
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    result = element_at(my_list, idx)
+    if result is not None:
+        print("Element at index {:d} is {}".format(idx, result))

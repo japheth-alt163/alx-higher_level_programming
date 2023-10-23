16f5067d5d42f2275a666b7b91760443b44ae1fd
japheth-alt163 <japhethbaraka4@gmail.com>
2023-10-23 07:03:30 -0700

    done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    elements_printed = 0
     try:
+        count = 0
         for i in range(x):
-            print(my_list[i], end='')
-            elements_printed += 1
+            print(my_list[i], end="")
+            count += 1
+        print()
     except IndexError:
-        pass
-    print()
-    return elements_printed
+        print()
+    return count
 
 
-# Test the function
-my_list = [1, 2, 3, 4, 5]
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+# Test the function with the provided example
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+
+    nb_print = safe_print_list(my_list, 2)
+    print("nb_print: {:d}".format(nb_print))
+
+    nb_print = safe_print_list(my_list, len(my_list))
+    print("nb_print: {:d}".format(nb_print))
+
+    nb_print = safe_print_list(my_list, len(my_list) + 2)
+    print("nb_print: {:d}".format(nb_print))
+

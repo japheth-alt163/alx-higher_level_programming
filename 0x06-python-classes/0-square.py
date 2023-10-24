74e2c23e41125f7b347e6ea12ad232f762973aa8
japheth-alt163 <japhethbaraka4@gmail.com>
2023-10-24 07:10:40 -0700

    trial

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    """
+    This is the Square class.
+
+    The Square class represents a square shape with equal sides.
+
+    Attributes:
+        No attributes are defined for this class.
+
+    Methods:
+        No methods are defined for this class.
+    """
+    pass

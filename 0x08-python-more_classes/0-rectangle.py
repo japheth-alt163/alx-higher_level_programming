6d63fa849f0028f6af950aada77c4c9b8326eb3a
japheth-alt163 <japhethbaraka4@gmail.com>
2023-10-30 04:39:26 -0700

    done

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,25 @@
 #!/usr/bin/python3
 class Rectangle:
-    pass
+    """
+    This class defines a rectangle.
+
+    Attributes:
+    - None
+    """
+    def __init__(self):
+        """
+        Initializes a new Rectangle instance.
+
+        Args:
+        - None
+
+        Returns:
+        - None
+        """
+        pass
+
+
+# Example usage:
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)

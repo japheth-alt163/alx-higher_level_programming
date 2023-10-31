9c84d634080b075874cf4f22c26a7b7328b1b216
japheth-alt163 <japhethbaraka4@gmail.com>
2023-10-31 08:24:14 -0700

    fire

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def magic_string():
+    if not hasattr(magic_string, 'count'):
+        magic_string.count = 0
+    else:
+        magic_string.count += 1
+    return "BestSchool, " * magic_string.count + "BestSchool"

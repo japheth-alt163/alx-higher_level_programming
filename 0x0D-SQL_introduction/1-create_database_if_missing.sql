3bf6949c509e556c4f7e0efca2efe30a14f101c1
japheth-alt163 <japhethbaraka4@gmail.com>
2023-12-13 06:45:35 -0800

    done

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,12 +1,4 @@
 -- Filename: 1-create_database_if_missing.sql
 
--- Check if the database exists
-SELECT COUNT(*) INTO @db_exists FROM information_schema.schemata WHERE schema_name = 'hbtn_0c_0';
-
--- If the database does not exist, create it
-SET @create_query = IF(@db_exists = 0, 'CREATE DATABASE hbtn_0c_0;', '');
-
--- Execute the create query
-PREPARE create_query FROM @create_query;
-EXECUTE create_query;
-DEALLOCATE PREPARE create_query;
+-- Creates a database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;

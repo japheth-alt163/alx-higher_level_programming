a2ff27724e1d44a10d25f005f99bd7a38df1daff
japheth-alt163 <japhethbaraka4@gmail.com>
2023-12-14 06:32:55 -0800

    done

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
-# Save the script in a file, e.g., 1-create_user.sql
+-- Save the script in a file, e.g., 1-create_user.sql
 
-# Content of 1-create_user.sql
+-- Content of 1-create_user.sql
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
-# Save the script in a file, e.g., 2-create_read_user.sql
+-- Save the script in a file, e.g., 2-create_read_user.sql
 
-# Content of 2-create_read_user.sql
+-- Content of 2-create_read_user.sql
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';

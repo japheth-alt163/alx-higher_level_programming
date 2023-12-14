94cea5e107fd2a0b0d9ab78f00aac6f63808f52b
japheth-alt163 <japhethbaraka4@gmail.com>
2023-12-14 06:20:48 -0800

    done

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+# Check privileges for user_0d_1
+echo "SHOW GRANTS FOR 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
+
+# Check privileges for user_0d_2
+echo "SHOW GRANTS FOR 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+# Save the script in a file, e.g., 2-create_read_user.sql
+
+# Content of 2-create_read_user.sql
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+
+FLUSH PRIVILEGES;

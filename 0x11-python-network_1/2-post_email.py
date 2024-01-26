7ab92a265729c1383f9ede1c11d4a3da0e787cf1
japheth-alt163 <japhethbaraka4@gmail.com>
2024-01-26 05:28:53 -0800

    mandatory

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
 """
-Takes a URL and an email as command-line arguments,
+Takes a URL and an email as command-line arguments, sends.
+a POST request, and displays the body of the response.
 """
 
-import urllib.request
 import urllib.parse
+import urllib.request
 import sys
 
-if len(sys.argv) != 3:
-    print("Usage: ./2-post_email.py <URL> <email>")
-    sys.exit(1)
 
-url = sys.argv[1]
-email = sys.argv[2]
+def post_email(url, email):
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    with urllib.request.urlopen(url, data=data) as response:
+        body = response.read().decode('utf-8')
+        print("Your email is:", body)
 
-# Encode the email as a parameter
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
-with urllib.request.urlopen(url, data=data) as response:
-    body = response.read().decode('utf-8')
-    print("Your email is:", body)
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: ./2-post_email.py <URL> <email>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    post_email(url, email)
